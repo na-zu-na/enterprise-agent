528c98d32256f88a67327061f05f32f9fb113315
na-zu-na <2553728769@qq.com>
2026-08-06 16:45:34 +0800

    完成了登录功能

---
+++ src/main/java/org/cc/enterpriseagent/auth/mapper/SysLoginLogMapper.java
@@ -0,0 +1,9 @@
+package org.cc.enterpriseagent.auth.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.cc.enterpriseagent.auth.entity.SysLoginLog;
+
+@Mapper
+public interface SysLoginLogMapper extends BaseMapper<SysLoginLog> {
+}
